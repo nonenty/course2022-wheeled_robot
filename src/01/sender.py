ae512544530deae70a86d541f1badef0d0cc65fa
mark <mark_zjunlict@zju.edu.cn>
2022-02-27 03:01:35 +0800

    init 01

---
+++ src/01/sender.py
@@ -0,0 +1,22 @@
+import socket
+from config import ENDPOINT as _ENDPOINT
+
+if __name__ == "__main__":
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def main1():
+        while True:
+            data = input()
+            sock.sendto(str.encode(data), _ENDPOINT)
+    
+    def main2():
+        from pynput.keyboard import Key, Listener
+
+        def on_press(key):
+            print("--------------")
+            # sock.sendto(str.encode(key), _ENDPOINT)
+        
+        with Listener(on_press=on_press) as listener:
+            listener.join()
+    # main1()
+    main2()
